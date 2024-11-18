9bffb78f97c9210dd1ac0d8ca08b0c29417a2d8a
Aditya kumar <ak6101283@gmail.com>
2024-11-18 12:15:46 +0530

    new modified files

--- src/autocomplete/todos/Todos.jsx
+++ src/autocomplete/todos/Todos.jsx
@@ -1,33 +1,44 @@
-
-
-const Todos = ({todos=[]}) => {
+import Button from "../button";
+import "./style.css";
+const Todos = ({ todos = [] , onEdit,onDel,}) => {
   return (
     <div>
       <h1>Your Todos</h1>
-      {todos.map((data,id)=>{ 
+      {todos.map((todos,id) => {
         return (
-          
-              <h2>
-        <Todoitem key={id} data={data}/>
-        </h2>
-        
-        
-        )
-})}
+          <h2>
+            <Todoitem
+              key={id}
+              todos={todos}
+              onEdit={onEdit}
+              onDel={onDel}
+            />
+          </h2>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
+
+const Todoitem = ({ todos, onDel, onEdit }) => {
+  function handleDel(id) {
+    return ()=>{ onDel(todos.id)};
+ 
+   }
+  function handelEdit(id) {
+    return  ()=> {onEdit(todos.id)};
+  }
 
 
-
-const Todoitem = ({data}) => {
+ 
   return (
     <div>
-      <span> {data.todo}</span>
+      <span> {todos.todo}</span>
+      <Button onClick={handleDel(todos.id)} label={"DEL"} />
+      <Button onClick={handelEdit(todos.id)} label={"Edit"} />
+      
     </div>
-  )
-}
-
-
+  );
+};
